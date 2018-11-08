0f2664b83ee6d8a3f042286d2cfc2eb8856878ce
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-08 14:52:48 +0530

    Update decimal_to_binary.py

--- stacks/decimal_to_binary.py
+++ stacks/decimal_to_binary.py
@@ -1,10 +1,14 @@
 import Stack #from stack ADT
 
 def divide_by_2(dec_number):
-    """ """
+    """ 
+    assumes dec_number is a potive integer
+    returns a binary of the number.
+    """
+    
     rem_stack = Stack()
     
-    while dec_number != 0:
+    while dec_number > 0:
         rem = dec_number % 2
         rem_stack.push(rem)
         dec_number = dec_number // 2
