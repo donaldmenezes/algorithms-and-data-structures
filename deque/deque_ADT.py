9cae57e540430060fc0787a20f1d9d8c5226b911
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2018-11-09 12:58:08 +0530

    Update deque_ADT.py

--- deque/deque_ADT.py
+++ deque/deque_ADT.py
@@ -9,7 +9,7 @@ class Deque:
         return self.items.append(item)
         
     def add_rear(self, item):
-        return self.item.insert(0, item)
+        return self.items.insert(0, item)
         
     def remove_front(self):
         return self.items.pop()
