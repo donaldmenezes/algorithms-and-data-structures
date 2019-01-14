ea71e47899edff1587ebcd1d74263f9403e477d8
donaldmenezes <43926873+donaldmenezes@users.noreply.github.com>
2019-01-14 09:57:50 +0530

    Ordered Sequential Search

---
+++ searching/ordered_sequential_search.py
@@ -0,0 +1,20 @@
+def ordered_sequential_search(alist, item):
+    
+    pos = 0
+    found = False
+    stop = False
+    
+    while pos < len(alist) and not found and not stop:
+        if alist[pos] == item:
+            found = True
+        elif alist[pos] > item:
+            stop = True
+        else:
+            pos = pos + 1
+    
+    return found
+    
+    
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(ordered_sequential_search(testlist, 3))
+print(ordered_sequential_search(testlist, 13))
